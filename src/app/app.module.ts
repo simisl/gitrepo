c938114102e9e1bb000a0c4344ef237134245ec0
simisl <76184760+simisl@users.noreply.github.com>
2022-05-17 12:56:22 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+interface personal{
+  avatar_url: string;
+  name: string;
+  html_url:string;
+  location:string;
+  bio:string;
+  twitter_username:string;
+  public_repos:number;
+}
+interface repo{
+  name:string;
+  description:string;
+  topics:string[];
+
+}
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements OnInit {
+  personalData!: personal;
+  personalRepo!: repo[];
+  count!: number ;
+  p: number = 1;
+  pageNumber!:number;
+  loading!: boolean;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.pageNumber = 1
+    var v = 'simisl'
+    this.http.get<personal>('https://api.github.com/users/i5ting ').subscribe(data=>{
+      this.personalData = data
+      this.count = this.personalData.public_repos;
+      console.log('data',data)
+    })
+      this.http.get<repo[]>(`https://api.github.com/users/i5ting/repos?q=addClass+i5ting:mozilla&per_page=10&page=${this.pageNumber}`).subscribe(data=>{
+
+      this.personalRepo = data
+      this.loading = false
+    })
+  }
+  pageChanged(pageNo: number,e:Event){
+    this.loading = true
+    console.log(pageNo,e)
+    this.pageNumber = pageNo;
+    this.http.get<repo[]>(`https://api.github.com/users/i5ting/repos?q=addClass+i5ting:mozilla&per_page=10&page=${this.pageNumber}`).subscribe(data=>{
+      this.personalRepo = data
+      this.loading = false
+    })
+
+  }
+}
